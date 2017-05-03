ce8e87e283916a9e7c13145ac7fc8793b6da6ea7
ziel5122 <azielinski@csumb.edu>
2017-05-02 23:26:02 -0700

    added mnist_preprocess back

---
+++ data/mnist_preprocess.py
@@ -0,0 +1,15 @@
+import idx2numpy
+from numpy import vstack
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets('./MNIST', one_hot=True)
+
+mnist_images = vstack((mnist.train.images, mnist.test.images,
+    mnist.validation.images))
+mnist_labels = vstack((mnist.train.labels, mnist.test.labels,
+    mnist.validation.labels))
+
+idx2numpy.convert_to_file('./MNIST/mnist-images.idx', mnist_images)
+idx2numpy.convert_to_file('./MNIST/mnist-labels.idx', mnist_labels)
+
+print('images:', len(mnist_images), '/ labels:', len(mnist_labels))
